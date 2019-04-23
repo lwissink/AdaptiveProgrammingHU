fe3e6fe9b3c9724b467a70165f0f05a932bd772d
lwissink <l.wissink1998@gmail.com>
2019-04-24 01:06:32 +0200

    test

--- Exercise8/Program.cs
+++ Exercise8/Program.cs
@@ -11,10 +11,24 @@ namespace Exercise8
             inputNum = Console.ReadLine();
             int num = int.Parse(inputNum);
             Console.WriteLine("You entered '{0}'", num);
-            for (int i = 0; i < num; i++)
+            
+            const string format = "    {0,5}";
+            
+            
+            for (int i = 0; i < num+1; i++)
             {
-                string sterretje = new String('*', num);
-                Console.WriteLine(sterretje);
+                string sterretje = new String('*', i);
+                Console.WriteLine(String.Format(format, sterretje));
+            }
+
+            for (int i = num-1; i < num; i--)
+            {
+                string sterretje = new String('*', i);
+                Console.WriteLine(string.Format(format, sterretje));
+                if (i == 0)
+                {
+                    break;
+                }
             }
         }
     }
