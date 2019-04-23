8880d1b32508c935da6bcd4815b276a8b7900300
lwissink <l.wissink1998@gmail.com>
2019-04-23 15:50:05 +0200

    exercise 3

--- Exercise6/Program.cs
+++ Exercise6/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Exercise6
+﻿using System;
+
+namespace Exercise6
 {
     internal class Program
     {
+        public static int ears(int n_rabbits){
+            if(n_rabbits <= 1){
+                return n_rabbits * 2;
+            }else{
+                return 2 + ears(n_rabbits-1); //hier word het opnieuw aangeroepen
+            }
+        }
         public static void Main(string[] args)
         {
+            Console.WriteLine(ears(4)); 
         }
     }
 }
