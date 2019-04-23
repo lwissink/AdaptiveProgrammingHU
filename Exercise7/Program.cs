769f7149587aaf0f52051cc70507049075f28b1e
lwissink <l.wissink1998@gmail.com>
2019-04-23 15:50:29 +0200

    exercise 3

---
+++ Exercise7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise7
+{
+    internal class Program
+    {
+        public static int findGCD(int a, int b)
+        {
+            if( a == b){
+                return a;
+            }
+            else if( a>b ){
+                return findGCD(a-b, b);
+            }else{
+                return findGCD(a, b-a);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(findGCD(900,280));
+            //900 - 280
+            //620 - 280
+            //340 - 280
+            //280 - 60
+            //220 - 60
+            //160 - 60
+            //100 - 60
+            //60 - 40
+            //20
+            //dus 9 keer
+        }
+    }
+}
---
+++ Exercise8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputNum;
+            Console.Write("Enter a number - ");
+            inputNum = Console.ReadLine();
+            int num = int.Parse(inputNum);
+            Console.WriteLine("You entered '{0}'", num);
+            for (int i = 0; i < num; i++)
+            {
+                string sterretje = new String('*', num);
+                Console.WriteLine(sterretje);
+        }
+    }
+}
