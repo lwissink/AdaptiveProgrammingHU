cae9436d9acd60c1b8e680c1bcc4850b8e781558
lwissink <l.wissink1998@gmail.com>
2019-04-23 03:01:44 +0200

    test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String word = "sterk";
+            String part = "ste";
+            String notpart = "str";
+            
+            Console.WriteLine(word.Contains(part));
+            Console.WriteLine(word.Contains(notpart));
+            
+            
+        }
+    }
+}
